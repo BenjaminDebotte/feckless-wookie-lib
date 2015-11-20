08b9dab00afc0ff5e103aa12d06c3242155d8118
Benjamin Debotté <benjamin.debotte@ecole.ensicaen.fr>
2015-11-20 17:23:10 +0100

    Ajout de la classe d'initialisation pour se connecter à la base de donnée

--- src/db/DbManagement.java
+++ src/db/DbManagement.java
@@ -1,5 +1,41 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbManagement {
+
+	public static void initDBDrivers(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (Exception e){
+				System.err.println("Impossible de charger le driver " + e.getMessage ());
+				System.exit (-1) ; // ou throw new RuntimeException("Impossible de charger le driver"); dans le cas d’un développement web
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		initDBDrivers();
+		
+		// TODO Auto-generated method stub
+		final String NomBase = "jdbc:mysql://localhost/si";
+		final String User = "si";
+		final String Password = "Password1234";
+		Connection conn;
+		try {
+		conn = DriverManager.getConnection(NomBase, User, Password);
+		 // ou getConnection(NomBase+”?user=”+User+”&password=”+pass);
+		if (conn == null) {
+		System.out.println("Could not connect !!");
+		}
+		} catch (SQLException e) {
+		System.err.println("Erreur de connexion " + e.getMessage ()) ;
+		}
+	}
+	
+	
 
 }
