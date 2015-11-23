910c04f1f2f3cc96f9d13b40f757e2d38a3d42fc
Benjamin Debotté <benjamin.debotte@ecole.ensicaen.fr>
2015-11-23 16:32:27 +0100

    Développement de la classe DbManagement. Suppression de MysqlDbManagement qui n'a pas vocation d'être dans ce package.

--- src/db/DbManagement.java
+++ src/db/DbManagement.java
@@ -1,20 +1,38 @@
 package db;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class DbManagement {
+public class DbManagement implements IDbManagement {
 
+	private DbManagement instance = null;
+	private IDbManagement delegate = null;
 	
 	
-	public static void main(String[] args) {
-		
-		
-		
-		
+	public DbManagement getInstance() {
+		if(instance == null)
+			instance = new DbManagement();
+		return instance;
 	}
 	
-	
+	public void setDelegate(IDbManagement delegate) {
+		this.delegate = delegate;
+	}
+
+	@Override
+	public void connexion(String bdd_path) throws SQLException {
+		this.delegate.connexion(bdd_path);
+	}
+
+	@Override
+	public void deconnexion() {
+		this.delegate.deconnexion();
+		
+	}
+
+	@Override
+	public ResultSet query(String query) throws SQLException {
+		return this.delegate.query(query);
+	}
 
 }
