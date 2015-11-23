6709f8ebafd7bd9ecee71a5f6182bfc0bb6f83ff
Benjamin Debotté <benjamin.debotte@ecole.ensicaen.fr>
2015-11-23 17:31:01 +0100

    Ajout de précision sur la nature de l'exception

--- src/db/ADbManagement.java
+++ src/db/ADbManagement.java
@@ -5,22 +5,23 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.MissingResourceException;
 
 public class ADbManagement implements IDbManagement {
 
 	private Connection conn = null;
 	
-	private static void initDBDrivers() throws RuntimeException {
+	private static void initDBDrivers() throws MissingResourceException {
 		try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
 		} catch (Exception e){
 			System.err.println("Impossible de charger le driver " + e.getMessage ());
-			throw new RuntimeException("Impossible de charger le driver");
+			throw new MissingResourceException("Impossible de charger le driver", "com.mysql.jdbc.Driver", null);
 		}
 	}
 	
 	@Override
-	public void connexion(String bdd_path) throws SQLException, RuntimeException {
+	public void connexion(String bdd_path) throws SQLException, MissingResourceException {
 		
 		initDBDrivers();
 		
